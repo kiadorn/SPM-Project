b5ba4692110cc5cf4e88b5854c074ccc0612a494
Alexander Hall <alexander.hall.lanerfeldt@gmail.com>
2018-04-18 12:51:19 +0200

    Första AI + _MittEget folder
    Gör en folder i Assets som heter "_MittEget" så pushas/pullas den INTE av GitHub. Användbar för saker man vill ha temporärt för sig själv.

---
+++ SPM Project/Assets/Scripts/Enemy/PatrolEnemyBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolEnemyBehaviour : MonoBehaviour {
+
+    public float speed;
+    public float groundCheckDistance;
+
+    public bool movingRight = true;
+
+    public Transform groundDetection;
+
+    private Vector2 vectorDirection;
+
+    private void Start()
+    {
+        /*if (movingRight)
+        {
+            vectorDirection = Vector2.right;
+        } else
+        {
+            vectorDirection = Vector2.left;
+        }*/
+        vectorDirection = (movingRight == true) ? Vector2.right : Vector2.left;
+    }
+
+    void Update () {
+        transform.Translate(vectorDirection * speed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, groundCheckDistance);
+        if (groundInfo.collider == false) {
+            if (movingRight)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            } else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
+
+	}
+}
