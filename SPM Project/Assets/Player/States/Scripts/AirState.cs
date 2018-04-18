c02d1858bbd022b19687e76bb1356c8b21240110
JockeNord <joakim.t.n@gmail.com>
2018-04-18 12:34:48 +0200

    Se description
    Lagt till ny scen, och diverse scripts for character controller

---
+++ SPM Project/Assets/Player/States/Scripts/AirState.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+[CreateAssetMenu(menuName = "Player/States/Air")]
+public class AirState : State{
+	[Header("Movement")]
+	public float dashDistance;
+	public float Acceleration;
+	public float Friction;
+
+	public bool CanCancelJump;
+	public bool canDash;
+	private float FastFallModifier = 2f;
+	private PlayerController _controller;
+	private Transform transform { get { return _controller.transform; }}
+	private Vector2 Velocity { get { return _controller.Velocity; }
+		set{ _controller.Velocity = value; } }
+	
+	public override void Initialize(Controller owner)
+	{
+		_controller = (PlayerController)owner;
+	}
+
+	public override void Update()
+	{
+		UpdateDash ();
+		UpdateGravity();
+		RaycastHit2D[] hits = _controller.DetectHits();
+		UpdateNormalForce(hits);
+		transform.Translate(Velocity * Time.deltaTime);
+		CancelJump ();
+	}
+
+	private void UpdateMovement()
+	{
+		float input = Input.GetAxisRaw("Horizontal");
+		if (Mathf.Abs(input) > _controller.InputMagnitudeToMove)
+		{
+			Vector2 delta = Vector2.right * input * Acceleration * Time.deltaTime;
+			if (Mathf.Abs ((_controller.Velocity + delta).x) < _controller.MaxSpeed || Mathf.Abs (Velocity.x) > _controller.MaxSpeed && Vector2.Dot (Velocity.normalized, delta) < 0.0f) {
+				_controller.Velocity += delta;
+			} else {
+				_controller.Velocity.x = MathHelper.Sign (input) * _controller.MaxSpeed;
+			}
+		}
+		else
+		{
+			Vector2 currentDirection = Vector2.right * MathHelper.Sign(Velocity.x);
+			float horizontalVelocity = Vector2.Dot(Velocity.normalized, currentDirection) *
+				Velocity.magnitude;
+			Velocity -= currentDirection * horizontalVelocity * Friction * Time.deltaTime;
+		}
+	}
+
+	private void CancelJump()
+	{
+		float minJumpVelocity = _controller.GetState<GroundState>().JumpVelocity.Min;
+		if (Velocity.y < minJumpVelocity) {
+			CanCancelJump = false;
+		}
+		if (!CanCancelJump || Input.GetButton ("Jump")) {
+			return;
+		}
+		CanCancelJump = false;
+		_controller.Velocity.y = minJumpVelocity;
+	}
+
+	private void UpdateGravity()
+	{
+		float gravityModifier = Velocity.y < 0.0f ? FastFallModifier : 1f;
+		Velocity += Vector2.down * _controller.Gravity * gravityModifier * Time.deltaTime;
+	}
+
+	private void UpdateNormalForce(RaycastHit2D[] hits)
+	{
+		if (hits.Length == 0) return;
+		_controller.SnapToHit(hits[0]);
+		foreach (RaycastHit2D hit in hits)
+		{
+			Velocity += MathHelper.GetNormalForce(Velocity, hit.normal);
+			if (MathHelper.CheckAllowedSlope(_controller.SlopeAngles, hit.normal))
+				_controller.TransitionTo<GroundState>();
+			
+			if (MathHelper.GetWallAngleDelta(hit.normal) < _controller.MaxWallAngleDelta
+				&& Vector2.Dot((hit.point - (Vector2)transform.position).normalized,
+					Velocity.normalized) > 0.0f)
+				_controller.TransitionTo<WallState>();
+		}
+	}
+
+	public void UpdateDash()
+	{
+		if (!Input.GetButtonDown ("Jump") || canDash == false) {
+			return;
+		} else if (Input.GetButton ("Right") && Input.GetButtonDown ("Jump")) {
+			transform.position += Vector3.right * dashDistance;
+		} else if (Input.GetButton ("Left") && Input.GetButtonDown ("Jump")) {
+			transform.position += Vector3.left * dashDistance;
+		} else if (Input.GetButton ("Up") && Input.GetButtonDown ("Jump")) {
+			transform.position += Vector3.up * dashDistance;
+		} else if (Input.GetButton ("Down") && Input.GetButtonDown ("Jump")) {
+			transform.position += Vector3.down * dashDistance;
+		} else if ((Input.GetButton ("Right") && Input.GetButton ("Up")) && Input.GetButtonDown ("Jump")) {
+			transform.position += new Vector3(1,1,0) * dashDistance;
+		} else if ((Input.GetButton ("Right") && Input.GetButton ("Down")) && Input.GetButtonDown ("Jump")) {
+			transform.position += new Vector3(1,-1,0) * dashDistance;
+		} else if ((Input.GetButton ("Left") && Input.GetButton ("Up")) && Input.GetButtonDown ("Jump")) {
+			transform.position += new Vector3(-1,1,0) * dashDistance;
+		} else if ((Input.GetButton ("Left") && Input.GetButton ("Down")) && Input.GetButtonDown ("Jump")) {
+			transform.position += new Vector3(-1,-1,0) * dashDistance;
+		} 
+
+	}
+}
---
+++ SPM Project/Assets/Player/States/Scripts/GroundState.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Player/States/Ground")]
+public class GroundState : State{
+	private PlayerController _controller;
+
+	[Header("Movement")]
+	public float Acceleration;
+	public float Deceleration;
+	public float TurnModifier;
+	private Vector2 _groundNormal;
+
+	[Header("Jumping")]
+	public float InitialJumpDistance;
+	public MinMaxFloat JumpVelocity;
+	public MinMaxFloat JumpHeight;
+	public float TimeToJumpApex;
+	public int MaxJumps = 1;
+	private int _jumps;
+
+	private Vector2 VectorAlongGround { get { return
+			MathHelper.RotateVector(_groundNormal, -90f);} }
+	private Transform transform { get { return _controller.transform; }}
+	private Vector2 Velocity { get { return _controller.Velocity; }
+		set{ _controller.Velocity = value; } }
+
+
+	public override void Initialize(Controller owner){
+		_controller = (PlayerController)owner;
+		_controller.Gravity = (2 * JumpHeight.Max) / Mathf.Pow(TimeToJumpApex, 2);
+		JumpVelocity.Max = _controller.Gravity * TimeToJumpApex;
+		JumpVelocity.Min = Mathf.Sqrt(2 * _controller.Gravity * JumpHeight.Min);
+	}
+
+	public override void Enter()
+	{
+		_controller.GetState<AirState> ().canDash = true;
+		_jumps = MaxJumps;
+	}
+	public override void Update()
+	{
+		UpdateGravity();
+		RaycastHit2D[] hits = _controller.DetectHits(true);
+		if (hits.Length == 0){
+			_controller.TransitionTo<AirState>();
+			return;
+		}
+		UpdateGroundNormal(hits);
+		UpdateMovement();
+		UpdateNormalForce(hits);
+		transform.Translate(Velocity * Time.deltaTime);
+		UpdateJump();
+	}
+	public void UpdateJump()
+	{
+		if (!Input.GetButtonDown("Jump") || _jumps <= 0) return;
+		transform.position += Vector3.up * InitialJumpDistance;
+		_controller.Velocity.y = JumpVelocity.Max;
+		_jumps--;
+		_controller.GetState<AirState>().CanCancelJump = true;
+		if(!(_controller.CurrentState is AirState)) _controller.TransitionTo<AirState>();
+	}
+	private void UpdateGravity()
+	{
+		Velocity += Vector2.down * _controller.Gravity * Time.deltaTime;
+	}
+	private void UpdateGroundNormal(RaycastHit2D[] hits)
+	{
+		int numberOfGroundHits = 0;
+		_groundNormal = Vector2.zero;
+		foreach (RaycastHit2D hit in hits)
+		{
+			if (!MathHelper.CheckAllowedSlope(_controller.SlopeAngles, hit.normal))
+				continue;
+			_groundNormal += hit.normal;
+			numberOfGroundHits++;
+		}
+		if (numberOfGroundHits == 0)
+		{
+			_controller.TransitionTo<AirState>();
+			return;
+		}
+		_groundNormal /= numberOfGroundHits;
+		_groundNormal.Normalize();
+	}
+	private void UpdateNormalForce(RaycastHit2D[] hits)
+	{
+		if (hits.Length == 0) return;
+		_controller.SnapToHit(hits[0]);
+		foreach (RaycastHit2D hit in hits)
+		{
+			if (Mathf.Approximately(Velocity.magnitude, 0.0f)) continue;
+			Velocity += MathHelper.GetNormalForce(Velocity, hit.normal);
+		}
+	}
+	private void UpdateMovement()
+	{
+		if (_groundNormal.magnitude < MathHelper.FloatEpsilon) return;
+		float input = Input.GetAxisRaw("Horizontal");
+		if (Mathf.Abs(input) > _controller.InputMagnitudeToMove) Accelerate(input);
+		else Decelerate();
+	}
+	private void Accelerate(float input)
+	{
+		int direction = MathHelper.Sign(Velocity.x);
+		float turnModifier = MathHelper.Sign(input) != direction && direction != 0 ?
+			TurnModifier : 1f;
+		Vector2 deltaVelocity = VectorAlongGround * input * Acceleration * turnModifier
+			* Time.deltaTime;
+		Vector2 newVelocity = Velocity + deltaVelocity;
+		Velocity = newVelocity.magnitude > _controller.MaxSpeed ? VectorAlongGround *
+			MathHelper.Sign(Velocity.x) * _controller.MaxSpeed : newVelocity;
+	}
+	private void Decelerate()
+	{
+		Vector2 deltaVelocity = MathHelper.Sign(Velocity.x) * VectorAlongGround *
+			Deceleration * Time.deltaTime;
+		Vector2 newVelocity = Velocity - deltaVelocity;
+		Velocity = Velocity.magnitude < MathHelper.FloatEpsilon ||
+			MathHelper.Sign(newVelocity.x) != MathHelper.Sign(Velocity.x) ? Vector2.zero :
+			newVelocity;
+	}
+}
