c890fc1ee39ea2e3bebaf8d0ca4f5ed4d6dc2cab
JockeNord <joakim.t.n@gmail.com>
2018-04-18 15:34:39 +0200

    Dash flyttat från AirState till egen DashState
    DashState använder transform.translate istället för att endast ändra position nu.

---
+++ SPM Project/Assets/Player/States/Scripts/DashState.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Player/States/Dash")]
+public class DashState : State{
+	public int dashSpeed;
+
+	private Transform transform { get { return _controller.transform; }}
+	private PlayerController _controller;
+	private float originalGravity;
+
+	public override void Initialize(Controller owner){
+		_controller = (PlayerController)owner;
+	}
+
+	public override void Enter()
+	{
+		DisableGravity ();
+		Dash();
+		EnableGravity ();
+		//behöver check för att se om man dashat in i en vägg/golv
+		_controller.TransitionTo<AirState>();
+	}
+	public void DisableGravity(){
+		originalGravity = _controller.Gravity;
+		_controller.Gravity = 0;
+	}
+	public void Dash (){
+		float xdirandmag = Input.GetAxisRaw ("Horizontal") * dashSpeed;
+		float ydirandmag  = Input.GetAxisRaw ("Vertical") * dashSpeed;
+		transform.Translate(new Vector2(xdirandmag, ydirandmag ) * Time.deltaTime);
+	}
+	public void EnableGravity (){
+		_controller.Gravity = originalGravity;
+	}
+}
