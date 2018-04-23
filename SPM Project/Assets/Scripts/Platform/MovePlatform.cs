3193076364ffae4c7b7b8a554c90c0c18d0242e7
Alexander Hall <alexander.hall.lanerfeldt@gmail.com>
2018-04-23 14:38:50 +0200

    14:38 commit
    Scen för rotation, prefabs för basics, ändrat AirState values

--- SPM Project/Assets/Scripts/Platform/MovePlatform.cs
+++ SPM Project/Assets/Scripts/Platform/MovePlatform.cs
@@ -63,6 +63,19 @@ public class MovePlatform : MonoBehaviour {
         isWaiting = false;
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.SetParent(transform);
+        }
+    }
 
-
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
 }
---
+++ SPM Project/Assets/Scripts/Platform/MovePlatformAuto.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatformAuto : MonoBehaviour {
+
+    public float moveForwardSpeed;
+    public float moveBackSpeed;
+    [Range(0, 5)]
+    public float waitTime;
+
+    [HideInInspector]
+    public bool shouldIMove = false;
+    private Vector3 originalPos;
+    private bool goingUp = false;
+    private float timer;
+
+    private void Start()
+    {
+        originalPos = transform.position;
+    }
+
+    public void Move()
+    {
+
+        //Timer
+        if (transform.position == transform.parent.GetChild(1).transform.position)
+        {
+            goingUp = true;
+
+            timer += Time.deltaTime;
+            if (timer < waitTime)
+            {
+                return;
+            }
+            timer = 0;
+        }
+        else if (transform.position == originalPos)
+        {
+            goingUp = false;
+
+            timer += Time.deltaTime;
+            if (timer < waitTime)
+            {
+                return;
+            }
+            timer = 0;
+        }
+
+        //Rörelse
+        if (!goingUp)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, transform.parent.GetChild(1).transform.position, moveForwardSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, originalPos, moveBackSpeed * Time.deltaTime);
+        }
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.SetParent(transform);
+            GameObject pos = transform.GetChild(1).gameObject;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
+}
