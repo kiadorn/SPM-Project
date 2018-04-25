609451cb80304707a91f86a61d8760bf3e07b2dd
ZakeG <38471952+ZakeG@users.noreply.github.com>
2018-04-25 10:50:39 +0200

    fixat nyckelprefabben och portalerna
    Lagt till kommentarer hur man använder dem också.

--- SPM Project/Assets/Scripts/Key.cs
+++ SPM Project/Assets/Scripts/Key.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Key : MonoBehaviour {
-
+    public string LevelThatHasBeenCompleted; //skriv vilken nivå det här objektet tillhör i den instansen av prefabben
 	void OnTriggerEnter2D(Collider2D col) {
         if (col.gameObject.CompareTag("Player") ) {
+            Debug.Log("COLLIDING WITH PLAYER");
             this.gameObject.SetActive(false);
-            if (GameManager.instance.Level1Done == false)
+            if (GameManager.instance.Level1Done == false && LevelThatHasBeenCompleted == "1")
             {
                 GameManager.instance.Level1Done = true;
+                Debug.Log(GameManager.instance.Level1Done);
             }
-            if (GameManager.instance.Level1Done == true && GameManager.instance.Level2Done == false)
+            if (GameManager.instance.Level1Done == true && LevelThatHasBeenCompleted == "2")
             {
                 GameManager.instance.Level2Done = true;
+                Debug.Log(GameManager.instance.Level2Done);
+
             }
         }
     }
