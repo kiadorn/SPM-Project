1a1051b2b2f8a24eeaa0951e06a2a1858da57455
Alexander Hall <alexander.hall.lanerfeldt@gmail.com>
2018-05-01 13:43:52 +0200

    Hazard OnStay

--- SPM Project/Assets/Scripts/Platform/Hazard.cs
+++ SPM Project/Assets/Scripts/Platform/Hazard.cs
@@ -7,7 +7,7 @@ public class Hazard : MonoBehaviour {
     [Range(0, 2)]
     public int damageValue;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
