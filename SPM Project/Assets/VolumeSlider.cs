b64313ab6946f32579bbec17c8b4952a4a07a210
JockeNord <joakim.t.n@gmail.com>
2018-05-03 16:32:20 +0200

    MainMenu färdig, AudioSlider fungerar

---
+++ SPM Project/Assets/VolumeSlider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour {
+	public AudioMixer mixer;
+
+	public void SetMasterVolume(Slider slider){
+		mixer.SetFloat ("MasterVolume", slider.value);
+	}
+}
