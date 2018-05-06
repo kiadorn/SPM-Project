432fb7d4c338dc2bde8f12c8a135e1f35f6bcb55
JockeNord <joakim.t.n@gmail.com>
2018-05-06 02:40:22 +0200

    Audio Slider uppdateras för att visa vad ljudet är.

--- SPM Project/Assets/VolumeSlider.cs
+++ SPM Project/Assets/VolumeSlider.cs
@@ -6,8 +6,14 @@ using UnityEngine.UI;
 
 public class VolumeSlider : MonoBehaviour {
 	public AudioMixer mixer;
+	private float currentVolume;
 
 	public void SetMasterVolume(Slider slider){
 		mixer.SetFloat ("MasterVolume", slider.value);
+	}
+
+	private void Update(){
+		mixer.GetFloat ("MasterVolume", out currentVolume);
+		this.GetComponent<Slider> ().value = currentVolume;
 	}
 }
