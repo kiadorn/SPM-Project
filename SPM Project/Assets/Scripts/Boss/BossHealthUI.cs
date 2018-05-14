779cb2fff34d51bdda4e8db6b5410435348e3fe3
ZakeG <38471952+ZakeG@users.noreply.github.com>
2018-05-14 12:34:27 +0200

    basic boss healthbar
    jobbar på bättre

---
+++ SPM Project/Assets/Scripts/Boss/BossHealthUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHealthUI : MonoBehaviour {
+
+    public GameObject Hand;
+    public Slider HealthBar;
+    public int currentHealth;
+    // Use this for initialization
+	void Start () {
+	}
+    private float CalculateHealth()
+    {
+        currentHealth = Hand.gameObject.GetComponent<HandSmash>().CurrentHealth;
+        return currentHealth;
+    }
+	private void HealthUpdate()
+    {
+        HealthBar.value = CalculateHealth();
+        Debug.Log(currentHealth);
+    }
+	// Update is called once per frame
+	void Update () {
+        HealthUpdate();
+	}
+}
