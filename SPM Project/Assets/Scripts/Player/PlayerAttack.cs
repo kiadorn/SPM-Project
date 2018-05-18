dea62f6233e4c55f0cc2a155e3c7ac0976ec2d41
JockeNord <joakim.t.n@gmail.com>
2018-05-18 15:32:25 +0200

    Ny Attack metod.

--- SPM Project/Assets/Scripts/Player/PlayerAttack.cs
+++ SPM Project/Assets/Scripts/Player/PlayerAttack.cs
@@ -4,63 +4,61 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour {
 	public float attackCooldown;
-	private BoxCollider2D attackArc;
+	public bool attackUnlocked;
+
+	private PlayerController _controller;
 	private float attackTimeStamp;
 	private float xDir;
-	public GameObject spriteObject; //Testrad ta bort när spelaren har animation
-
-	private Transform spriteTransform; //Testrad ta bort när spelaren har animation
+	private Vector2 attackSize;
+	private BoxCollider2D playerCollider;
+	private PlayerController playerControllerScript;
+	private Vector2 attackDirection;
 	private AudioSource source;
+
 	[Header("AudioClips")]
 	public AudioClip Swing;
 
+	public override void Initialize(Controller owner){
+		_controller = (PlayerController)owner;
+	}
+
 	void Start () {
+		playerControllerScript = GetComponent<PlayerController>();
+		playerCollider = GetComponent<BoxCollider2D>();
+		attackSize = new Vector2 (playerCollider.size.x, playerCollider.size.y);
 		attackTimeStamp = attackCooldown;
-		attackArc = this.GetComponent<BoxCollider2D>();
 		source = GetComponent<AudioSource>();
 	}
 
-	void Update () {
+	void Update (){
+		if ((_controller.CurrentState is WallState)) {
+			xDir = -playerControllerScript.GetLastXDirection ();
+		} else {
+			xDir = playerControllerScript.GetLastXDirection ();
+		}
 
-		UpdateColliderPosition ();
-
-		if (attackTimeStamp <= attackCooldown) {
+		if (attackTimeStamp <= attackCooldown){
 			attackTimeStamp += Time.deltaTime;
 		}
-		if (Input.GetButtonDown ("Fire1") && attackTimeStamp >= attackCooldown) {
-			attackArc.enabled = true;
-			this.GetComponent<SpriteRenderer> ().enabled = true;
-			attackTimeStamp = 0;
-			source.clip = Swing;
-			source.Play ();
-		} else {
-			this.GetComponent<SpriteRenderer> ().enabled = false;
-		}
-		
-	}
 
-	//När spelkaraktären har en sprite och storlek måste 1.5f ändras till något mer passande värde. Nuvarande värde är endast temporärt för testning.
-	private void UpdateColliderPosition(){
-		if(attackArc.enabled == true){
-		attackArc.enabled = false;
-		}
-		xDir = Input.GetAxisRaw ("Horizontal");
-		if(xDir > 0){
-			attackArc.transform.localPosition = new Vector2 (1.5f, 0f);
-		}else if(xDir < 0){
-			attackArc.transform.localPosition = new Vector2 (-1.5f, 0f);
-		}else if(xDir == 0){
-			return;
+		if (attackUnlocked) {
+			if(Input.GetButtonDown ("Fire1") && attackTimeStamp >= attackCooldown && ((_controller.CurrentState is WallState) || (_controller.CurrentState is AirState) || (_controller.CurrentState is GroundState))){
+				Attack ();
+				attackTimeStamp = 0f;
+			}
 		}
 	}
 
-	//Kallar på metoder för varje object i attackArc collidern när den aktiveras.
-	void OnTriggerEnter2D(Collider2D other){
-		if(other.tag == "Enemy"){
-			other.SendMessage ("TakeDamage", null, SendMessageOptions.DontRequireReceiver);
-		}if (other.tag == "Interactable") {
-			other.SendMessage ("Action", null, SendMessageOptions.DontRequireReceiver);
-		}
-			
+	private void Attack(){
+		source.PlayOneShot (Swing);
+		attackDirection = new Vector2 (xDir, 0);
+		RaycastHit2D[] hits = Physics2D.BoxCastAll ((Vector2)transform.position, attackSize, 0.0f, attackDirection, 2f);
+		foreach (RaycastHit2D hit in hits) {
+			if (hit.transform.tag == "Enemy") {
+				hit.transform.SendMessage ("TakeDamage", null, SendMessageOptions.DontRequireReceiver);
+			} else if (hit.transform.tag == "Interactable") {
+				hit.transform.SendMessage ("Action", null, SendMessageOptions.DontRequireReceiver);
+			}
+		}	
 	}
 }
